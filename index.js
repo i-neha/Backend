76ff5723503ea58b80e94e30be587fe8a9c5fd8b
Neha Sharma <i.nehaa98@gmail.com>
2023-03-31 02:20:46 +0530

    Revised Web Server

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const fs = require("fs");
+const http = require("http");
 
 /* Blocking Synchronous way: */
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+/* const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
 const textOut = `This is what we know about the avacado: ${textIn}.\nCreated on ${Date.now()}`;
 fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File Written");
+console.log("File Written"); */
 
 /* Non-Blocking Asynchronous way: */
 
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+/* fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
     console.log(data2);
 
@@ -24,3 +25,13 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
 });
 
 console.log("Will read file!");
+ */
+
+const server = http.createServer((req, res) => {
+  console.log(req);
+  res.end("Hello from the server!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Welcome to 8000");
+});
