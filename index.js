46b8f9b6f3200199fe22bcd6d499134cedd3b861
Neha Sharma <i.nehaa98@gmail.com>
2023-03-31 22:19:35 +0530

    Node Farm HTML Templating Done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 /* Blocking Synchronous way: */
 /* const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -27,9 +28,26 @@ console.log("File Written"); */
 console.log("Will read file!");
  */
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-  console.log(req);
-  res.end("Hello from the server!");
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the Overview");
+  } else if (pathName === "/product") {
+    res.end("This is the product");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
